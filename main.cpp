95fe2a7b9a57d39d01855b51963874f2f0456ac5
likojack <liko.jack@gmail.com>
2017-03-28 17:56:24 +1030

    toy SLAM initialize

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(int argc, char *argv[]) {
+    // input an initial position and a series of moving commands
+    // initial position in the format of (x,y,theta)
+    //command in the format of (r,d), d = moving length and r = rotation
+
+
+
+    return 0;
+}
